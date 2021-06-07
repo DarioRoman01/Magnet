d464a015825cb2cf750efe56d3d93dbac78fd62d
Haizza1 <darioroman2001@gmail.com>
2021-06-07 11:11:08 -0400

    create a litle cli app.

--- bin/Magnet.dart
+++ bin/Magnet.dart
@@ -1,6 +1,22 @@
+import 'dart:io';
+
 // ignore: library_prefixes
 import 'package:Magnet/Magnet.dart' as Magnet;
+import 'package:args/args.dart';
+
+const fileCmd = 'file';
+const replCmd = 'repl';
 
 void main(List<String> arguments) {
-  Magnet.start();
+  exitCode = 0;
+
+  final parser = ArgParser()..addFlag(fileCmd, negatable: true, abbr: 'f');
+  parser.addFlag(replCmd, negatable: true, abbr: 'r');
+  var argResult = parser.parse(arguments);
+  if (argResult[replCmd]) {
+    Magnet.startRpl();
+    return;
+  }
+
+  Magnet.read(argResult.rest[0]);
 }
--- lib/Magnet.dart
+++ lib/Magnet.dart
@@ -1,12 +1,48 @@
 import 'dart:io';
 import 'package:Magnet/evaluator/evaluator.dart';
 import 'package:Magnet/lexer/lexer.dart';
+import 'package:Magnet/lexer/token.dart';
 import 'package:Magnet/object/object.dart';
 import 'package:Magnet/parser/parser.dart';
-import 'package:Magnet/repl/repl.dart';
 
-void start() {
-  startRpl();
+final EOF_TOKEN = Token(TokenType.EOF, '');
+
+void startRpl() {
+  print('Welocme to Magnet!\n');
+  var scanned = <String>[];
+
+  while(true) {
+    stdout.write('>>> ');
+    var source = stdin.readLineSync();
+    if (source! == 'exit()') {
+      break;
+    }
+
+    scanned.add(source);
+    var lexer = Lexer(scanned.join(' '), '', 0, 0);
+    var parser = Parser(lexer);
+    var env = Enviroment(null);
+    var program = parser.parseProgram();
+
+    if (parser.errors.isNotEmpty) {
+      parser.errors.forEach((err) => print(err));
+      scanned.removeLast();
+      continue;
+    }
+
+    var evaluated = evaluate(program, env);
+    if (scanned.last.contains('escribir')) {
+      scanned.removeLast();
+    }
+
+    if (evaluated != SingletonNull) {
+      print(evaluated.inspect());
+
+      if (evaluated.runtimeType == Error) {
+        scanned.removeLast();
+      }
+    }
+  }
 }
 
 void read(String path) {
